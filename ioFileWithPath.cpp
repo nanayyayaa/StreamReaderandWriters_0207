1c4ed7550a1cafdd44b393ff8f66fe262275c244
nanayyayaa <jiejueejuju@gmail.com>
2026-06-01 20:16:52 +0700

    commit 11

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -4,4 +4,12 @@
 using namespace std;
 
 int main()
-{}
+{
+
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+}
