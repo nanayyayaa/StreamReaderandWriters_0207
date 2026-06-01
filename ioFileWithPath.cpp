df62f5de10dd7375ffeb758f3d375720e1f34ad4
nanayyayaa <jiejueejuju@gmail.com>
2026-06-01 20:38:01 +0700

    commit 17

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -51,4 +51,10 @@ int main()
         {
             cout << baris << endl;
         }
+        //menutup file jika telah selesai di loop
+        infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
 }
